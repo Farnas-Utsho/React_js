257f7eace116890df62af90d46a2f2ac9668ad27
Prince-Abir <mahmudabir886@gmail.com>
2024-04-14 14:27:41 +0600

    React basic

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from './Header';
+import "./About.css";
+
+function About() {
+    return (
+        <div className="About">
+           <Header />
+            
+            <h1> This is AboutPage</h1>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import "./Contact.css"
+import Header from './Header';
+
+function Contact() {
+    return (
+        <div className='Contact'>
+          <Header />  
+            <h1>This is Contact</h1>
+        </div>
+    )
+}
+
+export default Contact
